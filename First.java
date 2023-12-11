413df1bf9b116d4c3b850bc785e1b3eea0378210
deepaak682 <149398032+deepaak682@users.noreply.github.com>
2023-12-11 23:40:40 +0530

    Add files via upload
    Assignment to write the program to print 2 integers with the name of integers.

---
+++ First.java
@@ -0,0 +1,12 @@
+package java_practice;
+public class First
+{
+    public static void main(String args[])
+    {
+
+         int x=2;
+         int y=3;
+         System.out.println(x +" two");
+         System.out.println(y +" three");
+    }
+}
