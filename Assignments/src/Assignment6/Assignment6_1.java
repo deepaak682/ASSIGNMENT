f49c2bdf5ee1f68efdcd5310bb236c798c87eee2
deepa <deepa@DEEPA>
2023-12-19 12:07:48 +0530

    commit on 18-12-2023

---
+++ Assignments/src/Assignment6/Assignment6_1.java
@@ -0,0 +1,41 @@
+package Assignment6;
+
+class  MyDate
+{
+	private int dd,mm,yy;
+	static 
+	{
+		
+		
+		System.out.println("Reached here");
+		//System.out.println(this);
+		
+	}
+}
+public class Assignment6_1
+{
+	 public static void main(String[] args) 
+	 {
+
+ MyDate obj1= new MyDate();
+	 
+	 }		
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assignments/src/Assignment6/Assignment6_2.java
@@ -0,0 +1,23 @@
+package Assignment6;
+
+class Mydate1
+{
+	private int dd,mm,yy;
+	static void show()
+	{
+		
+		
+		System.out.println("Reached here");
+		//System.out.println(this);
+		
+	}
+}
+public class Assignment6_2
+{
+	 public static void main(String[] args) 
+	 {
+
+ Mydate1 obj1= new Mydate1();
+ Mydate1 .show();
+	 
+	 }}	
