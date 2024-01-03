81489fbc1257912ffc336fc342fcad7b35c6dd40
deepa <deepa@DEEPA>
2024-01-03 12:23:28 +0530

    commit on 3-01-2024

---
+++ Assignments/src/Assighnment9/MultiDimensionalStringArray.java
@@ -0,0 +1,31 @@
+package Assighnment9;
+
+public class MultiDimensionalStringArray {
+
+	public static void main(String[] args) 
+	{
+		String arr[][]= {
+				{"deepa","girish","ram"},
+				{"jhon","hari","sachu"},
+				{"manu","tomi","kishan"},
+		};
+		for(int i=0;i<arr.length;i++)
+		{
+			for (int j=0;j<arr.length;j++)
+			{
+	            if(arr[i][j]=="jhon")
+	            {
+	            	System.out.println(  arr[i][j]+ "found");
+	            }
+	           
+			}
+	            
+		}    
+	          
+	            
+	}		
+}		
+		
+
+	
+
--- Assignments/src/assignment8/stringMerge.java
+++ Assignments/src/assignment8/stringMerge.java
@@ -3,11 +3,11 @@ package assignment8;
 public class stringMerge {
 
 	public static void main(String[] args) {
-		String[] names = { "deepa","kishan","girish"};
+		String  names[] = { "deepa","kishan","girish"};
 	
 		int[] id= {10,11,12};
         
-		String first = names[0].concat(String.valueOf(id[0])); //  10 ->"10"
+		String first = names[0].concat(String.valueOf(id[0])); 
 		
 		String second = names[1].concat(String.valueOf(id[1])); 
 		
