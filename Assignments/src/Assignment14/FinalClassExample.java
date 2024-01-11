6b1a94cc8b308d4808b8ef5e9bdd0e69c89c90e8
deepa <deepa@DEEPA>
2024-01-11 23:58:45 +0530

    commit on 12-01-2024

--- Assignments/src/Assignment14/FinalClassExample.java
+++ Assignments/src/Assignment14/FinalClassExample.java
@@ -23,9 +23,10 @@ public class FinalClassExample
 
 	public static void main(String[] args) 
 	{
-		bikes h1=new heros();
+		heros h1=new heros();
 		h1.run();
-	   
+	   bikes b1=new bikes();
+	   b1.run();
 	  
 	}
 
