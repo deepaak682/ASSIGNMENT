052d10ce9cda4712c1200a9cc7c7d32b60d066d8
deepa <deepa@DEEPA>
2024-01-11 23:52:05 +0530

    commit on 11-01-2024

--- Assignments/src/Assignment14/FinalClassExample.java
+++ Assignments/src/Assignment14/FinalClassExample.java
@@ -18,14 +18,15 @@ class heros extends bikes
 	}
 }
 
-public class FinalClassExample {
+public class FinalClassExample 
+{
 
 	public static void main(String[] args) 
 	{
-		heros h1=new heros();
+		bikes h1=new heros();
 		h1.run();
-	    bikes b1=new bikes();
-	    b1.run();
+	   
+	  
 	}
 
 }
